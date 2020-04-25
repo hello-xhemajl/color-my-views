57ace460220041e85946ec04b1a566d8f1a5e117
xhemajl <xhemajl@post.com>
2020-04-25 15:18:16 +0200

    Add boxes

--- app/src/main/java/helo/mali/colormyviews/MainActivity.kt
+++ app/src/main/java/helo/mali/colormyviews/MainActivity.kt
@@ -1,12 +1,44 @@
 package helo.mali.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import helo.mali.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val boxes: List<View>
+        binding.apply {
+            boxes = listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText)
+        }
+
+        boxes.forEach { box ->
+            box.setOnClickListener { makeColored(it) }
+        }
+
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
